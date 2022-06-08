5dc6a53cf3c7b941157bbfa7bad944deb7f00142
Hieu Bui <hieu.bui@anphabe.com>
2022-06-08 17:39:44 +0700

    update

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ const io = new Server(server);
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/admin', (req, res) => {
+  res.sendFile(__dirname + '/admin.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  console.log('a user connected ' + socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected ' + socket.id);
+  });
 });
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-      io.emit('chat message', msg);
-    });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
+});
 server.listen(PORT, () => {
-    console.log(`listening on *: ${PORT}`);
+  console.log(`listening on *: ${PORT}`);
 });
